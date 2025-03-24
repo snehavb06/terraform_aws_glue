90275d6cb79dc564ba6e79bb9ca5123db5d76c9c
snehavb06 <146153718+snehavb06@users.noreply.github.com>
2025-03-24 21:32:03 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -22,3 +22,35 @@ resource "aws_iam_policy_attachment" "glue_policy" {
   roles      = [aws_iam_role.glue_role.name]
   policy_arn = "arn:aws:iam::aws:policy/AWSGlueConsoleFullAccess" "arn:aws:iam::aws:policy/AmazonS3FullAccess"
 }
+
+resource "aws_glue_job" "glue_etl" {
+  name     = var.glue_job_name
+  role_arn = aws_iam_role.glue_role.arn
+  glue_version = "3.0"
+  worker_type  = "G.1X"
+  number_of_workers = 2
+  timeout = 10
+
+  command {
+    name            = "gluestudio"
+    script_location = ""  # No script needed, Glue Studio manages it
+  }
+
+  default_arguments = {
+    "--TempDir"                 = "s3://glue-tutor-bucket-practice1/tmp/"
+    "--job-bookmark-option"     = "job-bookmark-enable"
+    "--enable-glue-datacatalog" = "true"
+    "--S3_SOURCE_PATH"          = var.s3_source_path
+    "--S3_TARGET_PATH"          = var.s3_target_path
+    "--S3_SOURCE_TYPE"          = "s3"
+    "--S3_SOURCE_FORMAT"        = "csv"
+    "--DELIMITER"               = ","
+    "--HAS_HEADER"              = "true"
+    "--TRANSFORM_TYPE"          = "dropDuplicates"
+    "--MATCH_ENTIRE_ROW"        = "true"
+  }
+
+  execution_property {
+    max_concurrent_runs = 1
+  }
+}
